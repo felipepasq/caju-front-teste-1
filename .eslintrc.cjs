ad9cb7c3ca817b3c246ec3040228c2fdd3b0454f
Felipe Pasqua <felipepasquarodrigues@gmail.com>
2024-07-20 18:59:25 -0300

    feat: added and configured prettier for format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,11 @@ module.exports = {
     'plugin:jsx-a11y/recommended',
     'plugin:@typescript-eslint/recommended',
     'eslint-config-prettier',
-    "plugin:react-hooks/recommended"
+    'plugin:react-hooks/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
   ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   settings: {
     react: {
       version: 'detect',
@@ -20,14 +23,15 @@ module.exports = {
     },
   },
   rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "react/react-in-jsx-scope": "off",
-    "import/no-named-as-default": 0,
-    "import/no-unresolved": "off",
-    "react/display-name": "off",
-    "@typescript-eslint/ban-types": "off",
-    "import/named": "off",
-    "react-hooks/exhaustive-deps": "warn",
-    "no-undef":"off"
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/no-named-as-default': 0,
+    'import/no-unresolved': 'off',
+    'react/display-name': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    'import/named': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-undef': 'off',
+    'prettier/prettier': 'error',
   },
 };
