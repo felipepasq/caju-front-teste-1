a1ed637d813469a3c4f4e4ff2693c52c32ce4bf3
Felipe Pasqua <felipepasquarodrigues@gmail.com>
2024-07-23 00:01:35 -0300

    feat: added Dashboard testsf

---
+++ src/pages/Dashboard/index.test.tsx
@@ -0,0 +1,37 @@
+import { queryRender } from '~/utils'
+import { DashboardPage } from '.'
+import { screen } from '@testing-library/react'
+import { toast } from 'react-toastify'
+
+jest.mock('react-router-dom', () => ({
+  useLocation: jest.fn().mockReturnValue({ search: undefined }),
+  useHistory: jest.fn().mockReturnValue({ replace: jest.fn() }),
+}))
+
+jest.mock('~/hooks', () => ({
+  useGetRegistrations: jest.fn().mockReturnValue({ isError: true }),
+  useSearchParams: jest.fn().mockReturnValue({ search: undefined }),
+}))
+
+jest.mock('react-toastify', () => ({
+  toast: {
+    success: jest.fn(),
+    error: jest.fn(),
+  },
+}))
+
+describe('Dashboard', () => {
+  it('Should render properly', () => {
+    queryRender(<DashboardPage />)
+    const text = screen.getByText('Pronto para revisar')
+    expect(text).toBeVisible()
+  })
+
+  it('Should show call error toast function', () => {
+    queryRender(<DashboardPage />)
+
+    expect(toast.error).toHaveBeenCalledWith(
+      'Ocorreu um erro ao carregar os registros.',
+    )
+  })
+})
